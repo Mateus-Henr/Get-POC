aca25430fa17ad74ce5b274a0d6f5f71e138e8fd
mateus <mateush.study@gmail.com>
2022-10-12 17:12:21 -0300

    Fix project files

---
+++ src/main/java/com/ufv/project/model/Student.java
@@ -0,0 +1,10 @@
+package com.ufv.project.model;
+
+public class Student extends User
+{
+    public Student(String firstName, String lastName, int CPF, String password)
+    {
+        super(firstName, lastName, CPF, password);
+    }
+
+}
