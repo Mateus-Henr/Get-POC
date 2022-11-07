9f1b8a8aeeb2448e731c7b9bf123be6077329ba2
Mateus-Henr <mateus.h.figueiredo@ufv.br>
2022-11-06 22:43:42 -0300

    Format code

--- src/main/java/com/ufv/project/Main.java
+++ src/main/java/com/ufv/project/Main.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 
 public class Main extends Application
 {
-
     @Override
     public void start(Stage stage) throws IOException
     {
--- src/main/java/com/ufv/project/model/Administrator.java
+++ src/main/java/com/ufv/project/model/Administrator.java
@@ -18,4 +18,5 @@ public class Administrator extends User
     {
         return true;
     }
+
 }
